3e6c5f167069df3073166b9ba98263f697255d31
Robert Matthew Johnson <matt@Roberts-MacBook-Pro.local>
2013-12-30 12:12:02 -0500

    Code for reproducing work in Chapter 2

---
+++ chapter2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+class Point():
+    def __init__(self):
+        self.Color = None
+        self.x = None
+        self.y = None
+
+def get_generated_points():
+    points = []
+    random.gauss(1.0, 
+    
+    np.array( [random.gauss(mu1[0], sigma1), random.gauss(mu1[1], sigma1)] ])
+    return points
+
+
+if __name__ == '__main__':
+    print 'test'
+
+
